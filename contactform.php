d538cc15ac7591c0ae8dcbc4fe16e0dd8785ecd8
Christopher Snead <cdsnead21@gmail.com>
2019-01-15 16:07:20 -0600

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -2,9 +2,9 @@
 if(isset($_POST['email'])) {
      
     // CHANGE THE TWO LINES BELOW
-    $email_to = "you@yourdomain.com";
+    $email_to = "cdsnead21@gmail.com";
      
-    $email_subject = "website html form submissions";
+    $email_subject = "Christopher Snead: Contact Form";
      
      
     function died($error) {
@@ -18,17 +18,13 @@ if(isset($_POST['email'])) {
      
     // validation expected data exists
     if(!isset($_POST['first_name']) ||
-        !isset($_POST['last_name']) ||
         !isset($_POST['email']) ||
-        !isset($_POST['telephone']) ||
         !isset($_POST['comments'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
      
     $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
     $email_from = $_POST['email']; // required
-    $telephone = $_POST['telephone']; // not required
     $comments = $_POST['comments']; // required
      
     $error_message = "";
@@ -39,9 +35,6 @@ if(isset($_POST['email'])) {
     $string_exp = "/^[A-Za-z .'-]+$/";
   if(!preg_match($string_exp,$first_name)) {
     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
   }
   if(strlen($comments) < 2) {
     $error_message .= 'The Comments you entered do not appear to be valid.<br />';
@@ -57,9 +50,7 @@ if(isset($_POST['email'])) {
     }
      
     $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
     $email_message .= "Comments: ".clean_string($comments)."\n";
      
      
