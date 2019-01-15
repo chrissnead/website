cb39739369420a94594d3feba1af4e8f0f995f3b
Christopher Snead <cdsnead21@gmail.com>
2019-01-15 16:13:03 -0600

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -17,15 +17,15 @@ if(isset($_POST['email'])) {
     }
      
     // validation expected data exists
-    if(!isset($_POST['first_name']) ||
+    if(!isset($_POST['name']) ||
         !isset($_POST['email']) ||
-        !isset($_POST['comments'])) {
+        !isset($_POST['message'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
      
-    $first_name = $_POST['first_name']; // required
+    $name = $_POST['name']; // required
     $email_from = $_POST['email']; // required
-    $comments = $_POST['comments']; // required
+    $message = $_POST['message']; // required
      
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -34,10 +34,10 @@ if(isset($_POST['email'])) {
   }
     $string_exp = "/^[A-Za-z .'-]+$/";
   if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+    $error_message .= 'The Name you entered does not appear to be valid.<br />';
   }
   if(strlen($comments) < 2) {
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+    $error_message .= 'The Message you entered does not appear to be valid.<br />';
   }
   if(strlen($error_message) > 0) {
     died($error_message);
@@ -49,9 +49,9 @@ if(isset($_POST['email'])) {
       return str_replace($bad,"",$string);
     }
      
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
+    $email_message .= "Name: ".clean_string($name)."\n";
     $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "Comments: ".clean_string($comments)."\n";
+    $email_message .= "Message: ".clean_string($message)."\n";
      
      
 // create email headers
@@ -63,7 +63,7 @@ $headers = 'From: '.$email_from."\r\n".
  
 <!-- place your own success html below -->
  
-Thank you for contacting us. We will be in touch with you very soon.
+Thank you for reaching out! I will be in touch with you very soon.
  
 <?php
 }
